3b8d6f81278c1bf590360975284b5c53bfef7408
Armchair Traveller <sentient.sober.armchair@gmail.com>
2022-08-25 16:20:10 -0700

    add fetching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,41 @@
 <!-- The home page is currently the listing page. -->
 <script>
+  import { getContextClient, queryStore, gql } from '@urql/svelte'
+
   import Fuse from 'fuse.js'
-  import homes from '../homes.json'
   import Filters from './Filters.svelte'
   import ListingGrid from './ListingGrid.svelte'
   import ListingItem from './ListingItem.svelte'
 
-  let filteredHomes = homes
+  // We're querying exactly what we need for this page. If this query is used in multiple places,
+  // we can move it to the gql folder and import it.
+  const homesStore = queryStore({
+    client: getContextClient(),
+    query: gql`
+      query GetAllHomes {
+        result: getAllHomes {
+          data {
+            _id
+            price
+            property {
+              _id
+              numberBedrooms
+              primaryImageUrl
+              description
+              address {
+                _id
+                city
+              }
+            }
+          }
+        }
+      }
+    `,
+  })
 
-  const fuse = new Fuse(homes, { keys: ['property.address.city'] })
+  $: homesData = $homesStore.data?.result?.data || []
+  $: filteredHomes = homesData
+  $: fuse = new Fuse(homesData, { keys: ['property.address.city'] })
 </script>
 
 <svelte:head>
@@ -25,44 +52,58 @@
       </p>
     </div>
     <Filters
-      cities={homes.map(
+      cities={homesData.map(
         ({
           property: {
             address: { city },
           },
         }) => city
       )}
-      on:filter={({ detail: data }) => {
-        // This is a bit of a hack, searching properties by city would normally be an API call, but we're fuzzy searching with fuse.js client-side for simplicity
-        // But the same could be true with price. Bigger datasets would require a server-side solution.
-        filteredHomes = (data.city ? fuse.search(data.city).map(({ item }) => item) : homes).filter(
-          ({ price, property: { numberBedrooms } }) => {
-            if (data.price.min && price < data.price.min) return
-            if (data.price.max && price > data.price.max) return
-            if (
-              data.bedrooms &&
-              ((data.bedrooms >= 6 && numberBedrooms < 6) || numberBedrooms != data.bedrooms)
-            )
-              return
-            return true
-          }
-        )
+      on:filter={function applyFilters({ detail: data }) {
+        // This is a bit of a hack, searching properties by city would normally be handled by an API call,
+        // whether through a database, or a specialized search like Algolia, Elastic, or TypeSense
+        // but we're fuzzy searching with fuse.js client-side for simplicity
+        // Bigger datasets would require a server-side solution, and an incomplete implementation with an overview
+        // can be found in src/lib/gql/fauna/setup.fql
+        filteredHomes = (
+          data.city ? fuse.search(data.city).map(({ item }) => item) : homesData
+        ).filter(({ price, property: { numberBedrooms } }) => {
+          if (data.price.min && price < data.price.min) return
+          if (data.price.max && price > data.price.max) return
+          if (
+            data.bedrooms &&
+            ((data.bedrooms >= 6 && numberBedrooms < 6) || numberBedrooms != data.bedrooms)
+          )
+            return
+          return true
+        })
       }}
     />
     <ListingGrid>
-      <!-- #each equiv `.map` w/ JSX in React e.g. `filteredHomes.map(({id, price, ...rest}) => (<div key={id}>{price}</div>))`,
-        the (id) is for keying the item -->
-      {#each filteredHomes as { id, price, property: { address: { city }, numberBedrooms: bedrooms, primaryImageUrl: src, description: alt } } (id)}
-        <ListingItem href="/home/{id}" {price} {city} {bedrooms} {src} {alt} />
-        <!-- Fallback if no items to display -->
-      {:else}
+      {#if $homesStore.fetching}
+        <!-- Might be jarring UX if data loads too fast, so a delay could help here. -->
         <div class="text-center py-16 px-4 sm:px-6 lg:px-8">
-          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900">No homes found</h1>
-          <p class="mt-4 max-w-xl mx-auto text-base text-gray-500">
-            We couldn't find any homes matching your criteria.
-          </p>
+          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900">Loading...</h1>
         </div>
-      {/each}
+      {:else if $homesStore.error}
+        <div class="text-center py-16 px-4 sm:px-6 lg:px-8">
+          <h1 class="text-4xl font-extrabold tracking-tight text-gray-900">Error</h1>
+        </div>
+      {:else}
+        <!-- #each equiv `.map` w/ JSX in React e.g. `filteredHomes.map(({id, price, ...rest}) => (<div key={id}>{price}</div>))`,
+          the (id) is for keying the item -->
+        {#each filteredHomes as { _id, price, property: { address: { city }, numberBedrooms: bedrooms, primaryImageUrl: src, description: alt } } (_id)}
+          <ListingItem href="/home/{_id}" {price} {city} {bedrooms} {src} {alt} />
+          <!-- Fallback if no items to display -->
+        {:else}
+          <div class="text-center py-16 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-4xl font-extrabold tracking-tight text-gray-900">No homes found</h1>
+            <p class="mt-4 max-w-xl mx-auto text-base text-gray-500">
+              We couldn't find any homes matching your criteria.
+            </p>
+          </div>
+        {/each}
+      {/if}
     </ListingGrid>
   </main>
 </div>
