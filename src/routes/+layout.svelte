ddb761915d4b83af16caf84fa464fd67235923bc
Armchair Traveller <sentient.sober.armchair@gmail.com>
2022-08-25 16:20:01 -0700

    init client

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
   import '../app.postcss'
   import Header from '../lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
+  import { setContextClient } from '@urql/svelte'
+  import { client } from '$lib/gql/client'
+
+  setContextClient(client)
 </script>
 
 <Header />
